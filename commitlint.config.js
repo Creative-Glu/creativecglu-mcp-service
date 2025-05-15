e9bf830dce18f1b9745637a38348568649552ff7
jaypeelaurence <jaypeelaurencecocjin@gmail.com>
2025-05-15 17:59:48 +0800

    chore: update package.json to add husky and commitlint dependencies, and configure prepare script

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0],
+    'body-max-line-length': [0],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'refactor', 'test', 'ci'],
+    ],
+  },
+};
